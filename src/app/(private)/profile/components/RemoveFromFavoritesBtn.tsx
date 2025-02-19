f3f4c945f42dafbb775623db09998debab9ab722
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-02-19 09:25:50 +0100

    Add loading spinner to RemoveFromFavoritesBtn
    Introduced a loading spinner and state management to enhance user feedback during an asynchronous operation. Replaced the direct `handleDislike` call with a wrapped `onClick` handler to manage pending states. This improves UX by indicating progress when removing an item from favorites.

--- src/app/(private)/profile/components/RemoveFromFavoritesBtn.tsx
+++ src/app/(private)/profile/components/RemoveFromFavoritesBtn.tsx
@@ -1,22 +1,36 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Trash2 } from "lucide-react";
 import { handleDislike } from "@/lib/actions";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 type Props = {
   productId: string;
 };
 
 const RemoveFromFavoritesBtn: React.FC<Props> = ({ productId }) => {
+  const [isPending, setIsPending] = useState<boolean>(false);
+
+  const onClick = async () => {
+    try {
+      setIsPending(true);
+      await handleDislike(productId);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
   return (
     <Button
       variant="ghost"
-      onClick={() => handleDislike(productId)}
+      onClick={onClick}
       className="text-red-500 hover:text-red-700"
     >
-      <Trash2 size={18} />
+      {isPending ? <LoadingSpinner /> : <Trash2 size={18} />}
     </Button>
   );
 };
